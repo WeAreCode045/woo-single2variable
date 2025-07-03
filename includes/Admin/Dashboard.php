bc6d3ce6af467dae61922ed9c4d3d2a37bbd60e8
WeAreCode045 <maurice@code045.nl>
2025-07-03 02:40:05 +0200

    C045

--- includes/Admin/Dashboard.php
+++ includes/Admin/Dashboard.php
@@ -15,13 +15,11 @@ class Dashboard {
         }
         wp_enqueue_style('select2', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css');
         wp_enqueue_script('select2', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', ['jquery']);
-        wp_enqueue_style('ws2v-admin', WS2V_PLUGIN_URL . 'assets/css/admin.css', [], WS2V_VERSION);
+        wp_enqueue_style('ws2v-admin', WS2V_PLUGIN_URL . 'assets/css/admin.css');
 wp_enqueue_script(
     'ws2v-admin-js',
     plugins_url('assets/js/admin.js', __FILE__),
-    ['jquery'],
-    filemtime(plugin_dir_path(__FILE__) . 'assets/js/admin.js'), // Use filemtime for version
-    true
+    ['jquery']
 );
         wp_localize_script('ws2v-admin', 'ws2v_ajax', [
             'nonce' => wp_create_nonce('ws2v_ajax_nonce')
